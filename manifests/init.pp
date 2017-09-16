3301c772856c31e6c32d3d3bf55775c4d86bc153
joeypiccola <joey.piccola@gmail.com>
2017-09-16 14:02:54 -0600

    init commit

---
+++ manifests/init.pp
@@ -0,0 +1,52 @@
+# == Class: kms_win
+#
+#   Manage the timezone on windows systems. Use WMI to verify what the timezone
+#   currently is and tzutil.exe to set it.
+#
+# === Parameters
+#
+# [*timezone*]
+#   The timezone to use. For a full list of available timezone run tzutil /l.
+#   Use the listed time zone ID (e.g. 'Eastern Standard Time')
+#
+# === Examples
+#
+#  class { kms_win:
+#    KeyManagementServiceName = 'kmsserver.contoso.com',
+#    KeyManagementServicePort = '1688',
+#  }
+#
+# === Authors
+#
+# Joey Piccola <joey@joeypiccola.com>
+#
+# === Copyright
+#
+# Copyright (C) 2016 Joey Piccola.
+#
+class kms_win (
+
+  $KeyManagementServiceName = undef,
+  $KeyManagementServicePort = 1688,
+  
+
+){
+
+  # parameter validation
+  validate_string($KeyManagementServiceName)
+  validate_re($KeyManagementServicePort, '\d+', 'KeyManagementServicePort parameter must be a number.')
+
+  exec { 'set-KeyManagementServiceName':
+    command  => "New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform" -Name KeyManagementServiceName -Value '${KeyManagementServiceName}' -PropertyType String -Force",
+    onlyif   => "if (((Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform').KeyManagementServiceName -eq '${KeyManagementServiceName}') -and (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform' | Select-Object -ExpandProperty 'KeyManagementServiceName' -ErrorAction Stop)) {exit 1}",
+    provider => powershell,
+  }
+
+
+  exec { 'set-KeyManagementServiceName':
+    command  => "New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform" -Name KeyManagementServicePort -Value '${KeyManagementServicePort}' -PropertyType String -Force",
+    onlyif   => "if (((Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform').KeyManagementServiceName -eq '${KeyManagementServicePort}') -and (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform' | Select-Object -ExpandProperty 'KeyManagementServicePort' -ErrorAction Stop)) {exit 1}",
+    provider => powershell,
+  }  
+
+}
